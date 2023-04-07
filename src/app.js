62f99664b36a1470c7a7cf4f9aa78fa95b991948
David Avery <avery.di@gmail.com>
2023-04-07 17:55:10 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ['the','my','your','our']
+let adjective = ['new','old','fast','cheap','best']
+let noun = ['pool','books','cars','pets','sports']
+for(let i = 0; i < pronoun.length; i++){
+  for(let j = 0; j < adjective.length; j++){
+    for(let k = 0; k < noun.length; k++){
+      console.log(`${pronoun[i]}${adjective[j]}${noun[k]}.com`)
+    }
+  }
+}
